33b1c561f23082e79c9740990756fed9774bcb95
CharlesChiuGit <charleschiu2012@gmail.com>
2025-01-28 03:53:23 +0800

    feat(zsh.hashdirs): update hashdirs

--- conf.d/zsh/hashdirs.zsh
+++ conf.d/zsh/hashdirs.zsh
@@ -1,10 +1,12 @@
 # enter hashdir via `cd ~XXX`
-hash -d work="$HOME/Workspace"
+hash -d work="$HOME/Work"
+hash -d Work="$HOME/Work"
 hash -d music="$HOME/Music"
 hash -d picture="$HOME/Pictures"
 hash -d download="$HOME/Downloads"
 hash -d document="$HOME/Documents"
 hash -d cfg="$HOME/.config"
+hash -d config="$HOME/.config"
 hash -d local="$HOME/.local"
 hash -d share="$HOME/.share"
 hash -d state="$HOME/.state"
