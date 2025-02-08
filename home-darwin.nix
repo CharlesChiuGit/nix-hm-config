1089f48645b1ff667729dce85ef67a58b4d3cac9
CharlesChiuGit <charleschiu2012@gmail.com>
2025-02-08 21:31:15 +0800

    feat: add `gh`, `gh-dash` and other gh extensions

--- home-darwin.nix
+++ home-darwin.nix
@@ -216,6 +216,9 @@ with lib; {
     delta = {
       enable = true;
     };
+    gh-dash = {
+      enable = true;
+    };
     lazygit = {
       enable = true;
     };
@@ -480,6 +483,38 @@ with lib; {
       extraConfig = ''
         ${builtins.readFile ./conf.d/tmux/tmux.conf}
       '';
+    };
+
+    gh = {
+      enable = true;
+      gitCredentialHelper = {
+        enable = true;
+        hosts = ["https://github.com"];
+      };
+      extensions = with pkgs; [
+        gh-actions-cache
+        gh-cal
+        gh-contribs
+        gh-copilot
+        gh-dash
+        gh-eco
+        gh-f
+        gh-i
+        gh-markdown-preview
+        gh-notify
+        gh-poi
+        gh-s
+        gh-screensaver
+      ];
+      settings = {
+        git_protocol = "ssh";
+        editor = "nvim";
+        aliases = {};
+      };
+    };
+
+    gh-dash = {
+      enable = true;
     };
 
     yazi = {
--- home.nix
+++ home.nix
@@ -211,6 +211,9 @@ with lib; {
     delta = {
       enable = true;
     };
+    gh-dash = {
+      enable = true;
+    };
     lazygit = {
       enable = true;
     };
@@ -342,6 +345,38 @@ with lib; {
         enableShellIntegration = true;
         shellIntegrationOptions = ["-d 60%"];
       };
+    };
+
+    gh = {
+      enable = true;
+      gitCredentialHelper = {
+        enable = true;
+        hosts = ["https://github.com"];
+      };
+      extensions = with pkgs; [
+        gh-actions-cache
+        gh-cal
+        gh-contribs
+        gh-copilot
+        gh-dash
+        gh-eco
+        gh-f
+        gh-i
+        gh-markdown-preview
+        gh-notify
+        gh-poi
+        gh-s
+        gh-screensaver
+      ];
+      settings = {
+        git_protocol = "ssh";
+        editor = "nvim";
+        aliases = {};
+      };
+    };
+
+    gh-dash = {
+      enable = true;
     };
 
     lazygit = {
