d7100de21d5c72beee2c8648e2c232624d9a6a3c
CharlesChiuGit <charleschiu2012@gmail.com>
2025-02-09 22:56:58 +0000

    fix(hm)

--- home.nix
+++ home.nix
@@ -35,12 +35,8 @@ with lib; {
     };
     gc = {
       automatic = true;
-      dates = "weekly";
+      frequency = "weekly";
       options = "--delete-older-than 7d --max-freed $((64 * 1024**3))";
-    };
-    optimise = {
-      automatic = true;
-      dates = ["weekly"];
     };
   };
 
