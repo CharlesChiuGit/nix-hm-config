e5c120f72ed4dd21ced26670a2a4916e8e8a5b76
CharlesChiuGit <charleschiu2012@gmail.com>
2025-06-04 11:07:23 +0800

    feat(hm.nvim): update config

--- home.nix
+++ home.nix
@@ -436,12 +436,46 @@ with lib; {
       ];
       extraWrapperArgs = [
         "--suffix"
+        "LIBRARY_PATH"
+        ":"
+        "${lib.makeLibraryPath [
+          # manylinux
+          pkgs.bzip2
+          pkgs.curl
+          pkgs.libsodium
+          pkgs.libssh
+          pkgs.libxml2
+          pkgs.openssl
+          pkgs.stdenv.cc.cc
+          pkgs.stdenv.cc.cc.lib
+          pkgs.util-linux
+          pkgs.xz
+          pkgs.zlib
+          pkgs.zstd
+          # Packages not included in `nix-ld`'s NixOSModule
+          pkgs.glib
+          pkgs.libcxx
+        ]}"
+        "--suffix"
         "PKG_CONFIG_PATH"
         ":"
         "${lib.makeSearchPathOutput "dev" "lib/pkgconfig" [
-          pkgs.stdenv.cc.cc
-          pkgs.zlib
+          # manylinux
+          pkgs.bzip2
+          pkgs.curl
+          pkgs.libsodium
+          pkgs.libssh
+          pkgs.libxml2
           pkgs.openssl
+          pkgs.stdenv.cc.cc
+          pkgs.stdenv.cc.cc.lib
+          pkgs.util-linux
+          pkgs.xz
+          pkgs.zlib
+          pkgs.zstd
+          # Packages not included in `nix-ld`'s NixOSModule
+          pkgs.glib
+          pkgs.libcxx
         ]}"
       ];
       extraPython3Packages = pyPkgs:
@@ -458,7 +492,6 @@ with lib; {
           fzy
         ];
     };
-
     skim = {
       enable = true;
     };
