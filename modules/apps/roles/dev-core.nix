1cdfb20991a0f07900bc815d31c6696af1e97e9f
CharlesChiuGit <charleschiu2012@gmail.com>
2025-10-09 19:55:47 +0800

    chore(apps.roles.dev-core): clean up

--- modules/apps/roles/dev-core.nix
+++ modules/apps/roles/dev-core.nix
@@ -2,33 +2,29 @@
 {
   packages = with pkgs; [
     # Good CLIs
-    difftastic # Syntax-aware diff
-
     eva # Calculator REPL
     fzy # Better fuzzy finder
-    jq # JSON parser
-    delta # Syntax-highlighting pager for git
-    git-lfs # Git extension for versioning large files
     git-ignore # Qucikly and easily fetch .gitignore templates from gitignore.io
+    jq # JSON parser
     # ripsecrets # Prevent committing secret keys into your source code
     # sd # Intuitive find & replace CLI (sed alternative)
     tree-sitter # Parser generator tool and an incremental parsing library
     xh # Friendly and fast tool for sending HTTP requests
 
     # pistol
-    hexyl # Command-line hex viewer
-    nur.repos.charmbracelet.glow # Render markdown on the CLI, with pizzazz
-    lnav # Logfile Navigator
     chafa # terminal image viewer
+    hexyl # Command-line hex viewer
+    lnav # Logfile Navigator
+    nur.repos.charmbracelet.glow # Render markdown on the CLI, with pizzazz
     poppler-utils # pdftotext
     yq-go # jq but for YAML, JSON, XML, CSV, TOML
 
     # nix language server, formatter, linter
-    nil # installed for crush
-    # alejandra
-    statix # lints and suggestions for the nix
+    # alejandra # The Uncompromising Nix Code Formatter
     deadnix # Scan Nix files for dead code
+    nil # installed for crush
     nixfmt-rfc-style # nixfmt was renamed to nixfmt-classic. The nixfmt attribute may be used for the new RFC 166-style formatter in the future, which is currently available as nixfmt-rfc-style
+    statix # lints and suggestions for the nix
 
   ];
 }
