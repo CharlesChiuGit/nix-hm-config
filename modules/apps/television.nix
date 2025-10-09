cad35b4cc76c3b9b55d41a7cd81f8f492b7eeddf
CharlesChiuGit <charleschiu2012@gmail.com>
2025-10-09 15:00:40 +0800

    chore(apps.television): set `nix-search-tv` as default

--- modules/apps/television.nix
+++ modules/apps/television.nix
@@ -2,5 +2,15 @@
   television = {
     enable = true;
     enableZshIntegration = true;
+    settings = {
+      default_channel = "nix-search-tv";
+      # keybindings = {
+      #   quit = [
+      #     "esc"
+      #     "ctrl-c"
+      #   ];
+      # };
+    };
+    # channels = { };
   };
 }
