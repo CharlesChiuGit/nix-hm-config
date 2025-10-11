3087a092cc36e123669b4a9b3f02c8feae7b3bc6
CharlesChiuGit <charleschiu2012@gmail.com>
2025-10-11 22:18:02 +0800

    perf(conf.zsh): update `hashdirs.zsh`

--- conf.d/zsh/hashdirs.zsh
+++ conf.d/zsh/hashdirs.zsh
@@ -1,15 +1,15 @@
 # enter hashdir via `cd ~XXX`
 hash -d work="$HOME/Work"
+hash -d work="$HOME/Workspace"
 hash -d Work="$HOME/Work"
 hash -d music="$HOME/Music"
-hash -d picture="$HOME/Pictures"
+hash -d pic="$HOME/Pictures"
 hash -d download="$HOME/Downloads"
-hash -d document="$HOME/Documents"
-hash -d cfg="$HOME/.config"
-hash -d config="$HOME/.config"
-hash -d local="$HOME/.local"
-hash -d share="$HOME/.share"
-hash -d state="$HOME/.state"
-hash -d cache="$HOME/.cache"
+hash -d doc="$HOME/Documents"
+hash -d cfg="$XDG_CONFIG_HOME"
+hash -d config="$XDG_CONFIG_HOME"
+hash -d share="$XDG_DATA_HOME"
+hash -d state="$XDG_STATE_HOME"
+hash -d cache="$XDG_CACHE_HOME"
 hash -d nvim="$XDG_CONFIG_HOME"/nvim
 hash -d manager="$XDG_CONFIG_HOME"/home-manager
