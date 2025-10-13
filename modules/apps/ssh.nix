f92a89c19bc57613fe655d55fa9cc0a2bb3a0adc
CharlesChiuGit <charleschiu2012@gmail.com>
2025-10-13 21:34:24 +0800

    chore(apps.ssh): typo

--- modules/apps/ssh.nix
+++ modules/apps/ssh.nix
@@ -5,7 +5,7 @@
     includes = [
       "~/.ssh/override_config"
       # "${config.age.secrets.ssh_host_config.path}" # `sunlei/zsh-ssh` can't resolve absolute path
-      "$~/.ssh/host_configuration"
+      "~/.ssh/host_configuration"
     ];
     matchBlocks."*" = {
       addKeysToAgent = "no";
